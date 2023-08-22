741d0f1e36b9c7bd20f9d1ad3de8e99257425e2d
Helder Victoretti <heldervictoretti@yahoo.com.br>
2023-08-22 17:26:19 +0100

    fixed the bookings controller and added index to view bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :pals
-
+  has_many :bookings
   # validates :username, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
