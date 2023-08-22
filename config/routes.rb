230e58c2464abbd0218a00b32299c90d44824e38
Helder Victoretti <heldervictoretti@yahoo.com.br>
2023-08-22 11:55:05 +0100

    installing missing gems

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,22 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:show]
+
+
+
+  end
+
 end
+
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   root to: "lists#index"
+#   resources :lists, except: [:edit, :update] do
+#     resources :bookmarks, only: [:new, :create]
+#     resources :reviews, only: :create
+#   end
+#   resources :bookmarks, only: :destroy
+#   resources :reviews, only: :destroy
+# end
