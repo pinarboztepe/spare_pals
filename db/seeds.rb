76e6e9bfc689b33e6883faf6367af4ee0f70758f
bruno keller <brunochaveskeller1102@outlook.com>
2023-08-22 11:46:54 +0100

    a meaningful message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-User.create(email: "visitor@lewagon.com", password: "123456789", username: "bobby")
