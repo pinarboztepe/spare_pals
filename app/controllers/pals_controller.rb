400a8eada8ad73011b5951d54d98bc9335cfdd19
Pinar Boztepe <pinarikamu@hotmail.com>
2023-08-22 14:43:36 +0100

    New pals created

--- app/controllers/pals_controller.rb
+++ app/controllers/pals_controller.rb
@@ -1,29 +1,31 @@
 class PalsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @pals = Pal.all
   end
 
-  # def show
-  #   @pal = Pal.find(params[:id])
-  # end
+  def show
+    @pal = Pal.find(params[:id])
+  end
 
-  # def new
-  #   @pal = Pal.new
-  # end
+  def new
+    @pal = Pal.new
+  end
 
-  # def create
-  #   @pal = Pal.new(list_params)
-  #   if @pal.save
-  #     redirect_to pal_path(@pal)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @pal = Pal.new(pal_params)
+    @pal.user = current_user
+    if @pal.save
+      redirect_to pal_path(@pal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # private
+  private
 
-  # def pal_params
-  #   params.require(:pal).permit(:name)
-  # end
+  def pal_params
+    params.require(:pal).permit(:name, :location, :price)
+  end
 end
