b1e86eabae1cf142712d811149eb696e8d280856
Maximilian Lana <maxlana@gmx.co.uk>
2023-08-24 12:25:14 +0100

    saving map progress

--- app/models/pal.rb
+++ app/models/pal.rb
@@ -1,4 +1,6 @@
 class Pal < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,16 @@ paul = User.create(email: "paulm@lewagon.com", password: "221133445", username: 
 ringo = User.create(email: "ringos@lewagon.com", password: "332244556", username: "Ringo")
 george = User.create(email: "georgeh@lewagon.com", password: "556677889", username: "George")
 
-Pal.create(name: "Dave", location: "Champ de Mars, 5 Av. Anatole France, 75007 Paris", price: 30, user: bobby, rating: 4)
-Pal.create(name: "Taylor", location: "Times Square, 42 Broadway, New York, NY 10036", price: 40, user: bobby, rating: 3)
-Pal.create(name: "Josh", location: "Piazza San Marco, 1 Piazzetta San Marco, 30124 Venice, Italy", price: 50, user: john, rating: 5)
-Pal.create(name: "Pat", location: "Red Square, 1 Krasnaya Ploshchad, Moscow 109012, Russia", price: 65, user: john, rating: 3)
-Pal.create(name: "Chris", location: "Sydney Opera House, Bennelong Point, Sydney NSW 2000, Australia", price: 27, user: paul, rating: 2)
-Pal.create(name: "Nate", location: "Shibuya Crossing, 2-1 Dogenzaka, Shibuya, Tokyo 150-0043, Japan", price: 70, user: paul, rating: 4)
-Pal.create(name: "Rami", location: "The Colosseum, Piazza del Colosseo, 1, 00184 Rome, Italy", price: 89, user: ringo, rating: 1)
-Pal.create(name: "William", location: "Christ the Redeemer, Corcovado Mountain, Rio de Janeiro - RJ, Brazil", price: 45, user: ringo, rating: 5)
-Pal.create(name: "Franz", location: "The Great Wall of China, Badaling, Yanqing District, Beijing, China", price: 80, user: george, rating: 5)
-Pal.create(name: "Krist", location: "Brandenburg Gate, Pariser Platz, 10117 Berlin, Germany", price: 57, user: george, rating: 3)
+Pal.create(name: "Dave", location: "Hoxton, London", price: 30, user: bobby, rating: 4)
+Pal.create(name: "Taylor", location: "Camden, London", price: 40, user: bobby, rating: 3)
+Pal.create(name: "Josh", location: "Battersea, London", price: 50, user: john, rating: 5)
+Pal.create(name: "Pat", location: "Peckham, London", price: 65, user: john, rating: 3)
+Pal.create(name: "Chris", location: "Greenwich, London", price: 27, user: paul, rating: 2)
+Pal.create(name: "Nate", location: "Westminster, London", price: 70, user: paul, rating: 4)
+Pal.create(name: "Rami", location: "Hammersmith, London", price: 89, user: ringo, rating: 1)
+Pal.create(name: "William", location: "Richmond, London", price: 45, user: ringo, rating: 5)
+Pal.create(name: "Franz", location: "Morden, London", price: 80, user: george, rating: 5)
+Pal.create(name: "Krist", location: "Walthamstow, London", price: 57, user: george, rating: 3)
 
 puts "DB filled with #{User.count} users, and #{Pal.count} pals."
 # pals.each do |pal|
